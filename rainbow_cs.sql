203cc3196cb9b4dc62aa8041d3b39022388adec0
Nico Izumi <99137032+NicoIzumi30@users.noreply.github.com>
2022-05-10 00:24:22 +0700

    Add files via upload

---
+++ rainbow_cs.sql
@@ -0,0 +1,143 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 09, 2022 at 07:18 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `rainbow_cs`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `biaya`
+--
+
+CREATE TABLE `biaya` (
+  `id_biaya` int(10) NOT NULL,
+  `jenis` varchar(50) NOT NULL,
+  `biaya` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `biaya`
+--
+
+INSERT INTO `biaya` (`id_biaya`, `jenis`, `biaya`) VALUES
+(2, 'Sepeda Motor', 10000),
+(5, 'Mobil', 25000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id_transaksi` int(10) NOT NULL,
+  `no_nota` varchar(10) NOT NULL,
+  `jenis` varchar(50) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `bayar` int(10) NOT NULL,
+  `kembali` int(10) NOT NULL,
+  `total` int(10) NOT NULL,
+  `tanggal` date NOT NULL,
+  `id_user` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`id_transaksi`, `no_nota`, `jenis`, `nama`, `bayar`, `kembali`, `total`, `tanggal`, `id_user`) VALUES
+(66, 'C001', 'Mobil', 'Rizki ', 50000, 25000, 25000, '2022-05-10', 1),
+(67, 'C002', 'Sepeda Montor', 'Yanto', 20000, 10000, 10000, '2022-05-10', 1),
+(68, 'C003', 'Mobil', 'Yanti', 26000, 1000, 25000, '2022-05-10', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` tinyint(2) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(35) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `alamat` varchar(150) NOT NULL,
+  `hp` varchar(20) NOT NULL,
+  `level` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_user`, `username`, `password`, `nama`, `alamat`, `hp`, `level`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'Administrator', 'Sukabumi', '085735501035', 1),
+(12, 'user', '202cb962ac59075b964b07152d234b70', 'Nico Izumi', 'Bandung', '018210831213', 2),
+(14, 'userA', '202cb962ac59075b964b07152d234b70', 'Rizky Nur', 'Black Hole', '0827812831', 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `biaya`
+--
+ALTER TABLE `biaya`
+  ADD PRIMARY KEY (`id_biaya`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id_transaksi`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `biaya`
+--
+ALTER TABLE `biaya`
+  MODIFY `id_biaya` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id_transaksi` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` tinyint(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
